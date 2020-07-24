57d0b5e93711f10576d165884591eb7269e6246b
David Fuentes <fuentesdt@gmail.com>
2020-07-24 12:53:24 -0500

    ENH: 2D

--- applymodel.m
+++ applymodel.m
@@ -5,9 +5,11 @@
 % outputpath - output path where files will be written
 function applymodel( inputniftifilepath, mynetwork, outputpath, gpuid ,ExecutionEnvironment  )
 
-disp( ['inputniftifilepath= ''',inputniftifilepath,''';']);      
-disp( ['mynetwork         = ''',mynetwork         ,''';']);      
-disp( ['outputpath        = ''',outputpath        ,''';']);  
+disp( ['inputniftifilepath  = ''',inputniftifilepath  ,''';']);      
+disp( ['mynetwork           = ''',mynetwork           ,''';']);      
+disp( ['outputpath          = ''',outputpath          ,''';']);  
+disp( ['gpuid               = ''',gpuid               ,''';']);  
+disp( ['ExecutionEnvironment= ''',ExecutionEnvironment,''';']);  
 
 gpuDevice(str2double(gpuid))
 
@@ -20,7 +22,17 @@ trainedNN = load(mynetwork )
 
 %% apply trained network to nifti image
 tStart = tic;
-tempSeg = semanticseg(niivolume ,trainedNN.net,'ExecutionEnvironment',ExecutionEnvironment, 'outputtype', 'uint8');
+if trainedNN.net.Layers(1).InputSize(3) == 1
+  disp('2D')
+  % TODO - mbhatter - vectorize
+  tempSeg = zeros(size(niivolume),'uint8');
+  for kkk = 1: info.ImageSize(3)
+    tempSeg(:,:,kkk) = semanticseg( niivolume(:,:,kkk) ,trainedNN.net,'ExecutionEnvironment',ExecutionEnvironment, 'outputtype', 'uint8');
+  end
+else
+  disp('3D')
+  tempSeg = semanticseg(niivolume ,trainedNN.net,'ExecutionEnvironment',ExecutionEnvironment, 'outputtype', 'uint8');
+end
 %tempSeg = segmentImagePatchwise(niivolume ,trainedNN.net, [256 256 144]);
 tEnd = toc(tStart)
 
