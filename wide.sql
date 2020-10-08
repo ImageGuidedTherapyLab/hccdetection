6619c5b58d8785aec9f9fe6cceeeda957a7d4446
David Fuentes <fuentesdt@gmail.com>
2020-10-08 13:13:34 -0500

    WIP: baseline difference

--- wide.sql
+++ wide.sql
@@ -1,16 +1,23 @@
 .mode csv
 .import LiverMRIProjectData/datakeyANON.csv  imaging
 .import LiverMRIProjectData/LiverMRIAnonymizationKey.csv  datekey
+-- select HCCDate from datekey;
 .headers on
 create table hccimaging  as
-select dk.Status,im.StudyDate,dk.HCCDate,
-       julianday(replace(dk.HCCDate, rtrim(dk.HCCDate, replace(dk.HCCDate, '/', '')), '')  || '-' || printf('%02d', cast(substr(dk.HCCDate, 0, instr(dk.HCCDate,'/')) as int)) || '-' || printf('%02d', cast(substr(substr(dk.HCCDate, instr(dk.HCCDate,'/')+1), 0, instr(substr(dk.HCCDate, instr(dk.HCCDate,'/')+1),'/')) as int) )) - julianday(substr(im.StudyDate, 1, 4) || '-' || substr(im.StudyDate, 5,2) || '-' || substr(im.StudyDate, 7,2)) diagnosticinterval,
+select dk.Status,
+       CASE WHEN dk.HCCDate = ""  THEN NULL
+       ELSE replace(dk.HCCDate, rtrim(dk.HCCDate, replace(dk.HCCDate, '/', '')), '')  || '-' || printf('%02d', cast(substr(dk.HCCDate, 0, instr(dk.HCCDate,'/')) as int)) || '-' || printf('%02d', cast(substr(substr(dk.HCCDate, instr(dk.HCCDate,'/')+1), 0, instr(substr(dk.HCCDate, instr(dk.HCCDate,'/')+1),'/')) as int) ) END  as HCCDate,
+       substr(im.StudyDate, 1, 4) || '-' || substr(im.StudyDate, 5,2) || '-' || substr(im.StudyDate, 7,2) as  StudyDate,
        im.StudyNumber,im.PatientNumber,im.SeriesDescription,
-CASE WHEN (im.seriesDescription like "t1_vibe_fs%Pre"   or im.seriesDescription like '3D_Thr_Pre')   THEN 'Pre'
-     WHEN (im.seriesDescription like "t1_vibe_fs%Art%"  or im.seriesDescription like '3D_Thr_Art')   THEN 'Art'
-     WHEN (im.seriesDescription like "t1_vibe_fs%30%"   or im.seriesDescription like '3D_Thr_Ven')   THEN 'Ven'
-     WHEN (im.seriesDescription like "t1_vibe_fs%60%"   or im.seriesDescription like '3D_Thr_Del%')  THEN 'Del'
-     WHEN (im.seriesDescription like "t1_vibe_fs%Post%" or im.seriesDescription like '3D_Thr_Port')  THEN 'Pst'
+CASE WHEN im.seriesDescription like "%vibe%" THEN 'vibe'
+     WHEN im.seriesDescription like "%Thr%"  THEN 'thrive'
+     WHEN im.seriesDescription like "%lava%" THEN 'lava'
+     ELSE NULL END AS Vendor,
+CASE WHEN (im.seriesDescription like "t1_vibe_fs%Pre"   or im.seriesDescription like '3D_Thr_Pre'  or im.seriesDescription = 'Ax LAVA BH'     )  THEN 'Pre'
+     WHEN (im.seriesDescription like "t1_vibe_fs%Art%"  or im.seriesDescription like '3D_Thr_Art'  or im.seriesDescription like '%Ph1%Ax LAVA%'  )  THEN 'Art'
+     WHEN (im.seriesDescription like "t1_vibe_fs%30%"   or im.seriesDescription like '3D_Thr_Ven'  or im.seriesDescription like '%Ph2%Ax LAVA%'  )  THEN 'Ven'
+     WHEN (im.seriesDescription like "t1_vibe_fs%60%"   or im.seriesDescription like '3D_Thr_Del%' or im.seriesDescription like '%Ph3%Ax LAVA%'  )  THEN 'Del'
+     WHEN (im.seriesDescription like "t1_vibe_fs%Post%" or im.seriesDescription like '3D_Thr_Port' or im.seriesDescription like '%Ax LAVA%DELAY%')  THEN 'Pst'
      ELSE NULL END AS ImageType,
        im.SeriesModality,im.seriesanonuid,im.niftifile 
 from datekey dk join imaging im on  dk.slicerID = im.PatientNumber ;
@@ -20,8 +27,11 @@ from datekey dk join imaging im on  dk.slicerID = im.PatientNumber ;
 -- FIXME - missing - select * from hccimaging   where patientnumber = 9;
 -- FIXME - missing - select * from hccimaging   where patientnumber = 12;
 
+-- julianday(replace(dk.HCCDate, rtrim(dk.HCCDate, replace(dk.HCCDate, '/', '')), '')  || '-' || printf('%02d', cast(substr(dk.HCCDate, 0, instr(dk.HCCDate,'/')) as int)) || '-' || printf('%02d', cast(substr(substr(dk.HCCDate, instr(dk.HCCDate,'/')+1), 0, instr(substr(dk.HCCDate, instr(dk.HCCDate,'/')+1),'/')) as int) )) - julianday(substr(im.StudyDate, 1, 4) || '-' || substr(im.StudyDate, 5,2) || '-' || substr(im.StudyDate, 7,2)) diagnosticinterval,
 create table widestudy  as
-SELECT   printf('BCM%04d%03d', cast(PatientNumber as int) , cast(StudyNumber as int) ) UID,  Status, ifnull(diagnosticinterval,"inf") diagnosticinterval,
+SELECT   printf('BCM%04d%03d', cast(PatientNumber as int) , cast(StudyNumber as int) ) UID, Vendor, Status, StudyDate,
+            CASE WHEN (HCCDate is not NULL  )  THEN julianday(HCCDate) - julianday(StudyDate)
+            ELSE "inf" END AS diagnosticinterval,
             max(CASE WHEN ImageType = 'Pre' THEN seriesanonuid ELSE NULL END)  Pre,
             max(CASE WHEN ImageType = 'Art' THEN seriesanonuid ELSE NULL END)  Art,
             max(CASE WHEN ImageType = 'Ven' THEN seriesanonuid ELSE NULL END)  Ven,
@@ -32,12 +42,15 @@ FROM        hccimaging
 where       ImageType is not null 
 GROUP BY    PatientNumber, StudyNumber
 ORDER BY    cast(PatientNumber as int) ASC, cast(StudyNumber as int) ASC;
+-- select HCCDate, StudyDate from hccimaging;
+-- select diagnosticinterval from widestudy  ;
 
 -- update min
 create table minwidestudy  as 
 select PatientNumber, min(StudyNumber) MinStudyNumber from widestudy group by Patientnumber;
 create table baselineart  as 
-select w1.PatientNumber, w1.Art from widestudy w1  join minwidestudy w2 on  w1.PatientNumber = w2.PatientNumber and w1.StudyNumber = w2.MinStudyNumber;
+select w1.PatientNumber, w1.Art, w1.StudyDate from widestudy w1  join minwidestudy w2 on  w1.PatientNumber = w2.PatientNumber and w1.StudyNumber = w2.MinStudyNumber;
+-- select * from baselineart  ;
 
 -- error check missing data
 select count(UID),count(Status),count(diagnosticinterval),count(Pre) ,count(Art) ,count(Ven),count(Del),count(Post)  from widestudy;
@@ -46,7 +59,7 @@ select count(UID),count(Status),count(diagnosticinterval),count(Pre) ,count(Art)
 
 -- output wide format
 .output LiverMRIProjectData/wideanon.csv 
-select w1.*,w2.MinStudyNumber,printf('BCM%04d%03d/%s', cast(w1.PatientNumber as int), cast(w2.MinStudyNUmber as int),w3.Art) Fixed
+select w1.*,w2.MinStudyNumber,julianday(w1.StudyDate)-julianday(w3.StudyDate) daysincebaseline,printf('BCM%04d%03d/%s', cast(w1.PatientNumber as int), cast(w2.MinStudyNUmber as int),w3.Art) Fixed
 from widestudy    w1
 join minwidestudy w2 on w1.PatientNumber = w2.PatientNumber 
 join baselineart  w3 on w1.PatientNumber = w3.PatientNumber;
