29f587533f3d8d5bb9e48e5a06320df47885c3e2
David Fuentes <fuentesdt@gmail.com>
2021-05-05 17:02:57 -0500

    WIP:

--- lrstats.R
+++ lrstats.R
@@ -4,6 +4,8 @@
 #ROC plots: https://github.com/EGates1/RadPath/blob/master/Code/GradePreds.R#L751
 #PR plot: https://github.com/EGates1/RadPath/blob/master/Code/GradePreds.R#L761
 
+graphics.off()
+
 # source('lrstats.R')
 fname <- "qastats/wide.csv" 
 mydataset <- read.csv(fname, na.strings=c(".", "NA", "", "?"), strip.white=TRUE, encoding="UTF-8")
@@ -12,15 +14,18 @@ mydataset <- read.csv(fname, na.strings=c(".", "NA", "", "?"), strip.white=TRUE,
 
 # Calculate the Area Under the Curve (AUC).
 
-myroc3 = pROC::roc( response = ifelse(mydataset$truth == 3,1,0), predictor = mydataset$predict3  , curve=T   )
-myroc4 = pROC::roc( response = ifelse(mydataset$truth == 4,1,0), predictor = mydataset$predict4  , curve=T   )
-myroc5 = pROC::roc( response = ifelse(mydataset$truth == 5,1,0), predictor = mydataset$predict5  , curve=T   )
 
+myroc3a = pROC::roc( response = ifelse(mydataset$truth == 3,1,0), predictor = mydataset$predict3  , curve=T   )
+myroc4a = pROC::roc( response = ifelse(mydataset$truth == 4,1,0), predictor = mydataset$predict4  , curve=T   )
+myroc5a = pROC::roc( response = ifelse(mydataset$truth == 5,1,0), predictor = mydataset$predict5  , curve=T   )
+myroc5b = pROC::roc( response = ifelse(mydataset$truth == 5,1,0), predictor = mydataset$countlr5  / mydataset$compsize, curve=T   )
+
+cbind(mydataset$InstanceUID,mydataset$LabelID, mydataset$countlr5, mydataset$compsize, mydataset$countlr5  / mydataset$compsize,mydataset$truth,ifelse(mydataset$truth == 5,1,0))
 # Calculate the AUC Confidence Interval.
 
-# pROC::ci.auc( mydataset$truth, mydataset$predict5  )
+pROC::ci.auc( ifelse(mydataset$truth == 5,1,0), mydataset$predict5  )
+pROC::ci.auc( ifelse(mydataset$truth == 5,1,0), mydataset$countlr5  / mydataset$compsize)
 
-plot(myroc3)
-plot(myroc4)
-plot(myroc5)
+plot(myroc3a);x11() 
+plot(myroc5b)
 
