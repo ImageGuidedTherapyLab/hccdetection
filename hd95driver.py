9cb2f4e10927c8d18201989c019dbf2b8536e9a8
David Fuentes <fuentesdt@gmail.com>
2022-02-08 17:47:22 -0600

    WIP:

--- hd95driver.py
+++ hd95driver.py
@@ -1,13 +1,17 @@
+#python hd95driver.py > hd95.csv
 import med_seg_metrics as ms
 import os
 uidlist = os.listdir('Processed')
 
 for myuid in uidlist:
    try:
-      myHD = ms.HausdorffDistance('Processed/%d/scaled/256/densenet3d/run_a/label.nii.gz' % int(myuid),'Processed/%d/scaled/256/densenet3d/run_a/liver.nii.gz' % int(myuid),'95')
-      print(myuid, myHD)
+      cpheadercmd = 'c3d Processed/%d/256/Truth.nii Processed/%d/scaled/256/densenet3d/run_a/liver.nii.gz -copy-transform -o Processed/%d/scaled/256/densenet3d/run_a/liverheader.nii.gz' % (int(myuid),int(myuid),int(myuid))
+      #print(cpheadercmd)
+      os.system(cpheadercmd)
+      myHD = ms.HausdorffDistance('Processed/%d/256/Truth.nii' % int(myuid),'Processed/%d/scaled/256/densenet3d/run_a/liverheader.nii.gz' % int(myuid),'95')
+      print(myuid,',',myHD)
    except ValueError:
-      print(myuid, 'is not an int')
+      print(myuid, 'notAnInt')
    except RuntimeError:
-      print(myuid, 'fail...')
+      print(myuid, 'fail')
 
