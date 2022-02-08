77dcbff75d38e5c3f350618dcb23d3f6091b9b6a
David Fuentes <fuentesdt@gmail.com>
2022-02-08 11:19:21 -0600

    WIP: HD

--- hd95driver.py
+++ hd95driver.py
@@ -4,11 +4,10 @@ uidlist = os.listdir('Processed')
 
 for myuid in uidlist:
    try:
-      ms.HausdorffDistance('Processed/%d/scaled/256/densenet3d/run_a/label.nii.gz' % int(myuid),'Processed/%d/scaled/256/densenet3d/run_a/liver.nii.gz' % int(myuid),'95')
+      myHD = ms.HausdorffDistance('Processed/%d/scaled/256/densenet3d/run_a/label.nii.gz' % int(myuid),'Processed/%d/scaled/256/densenet3d/run_a/liver.nii.gz' % int(myuid),'95')
+      print(myuid, myHD)
    except ValueError:
       print(myuid, 'is not an int')
    except RuntimeError:
       print(myuid, 'fail...')
-
-
 
