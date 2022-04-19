ce7b44fef1375dbd497f977c79f15f277b1d6c2c
David Fuentes <fuentesdt@gmail.com>
2022-04-19 16:12:28 -0500

    BUG: cnr update

--- epmstatistics.sql
+++ epmstatistics.sql
@@ -26,33 +26,62 @@ select ptid,UID,status from patientlistprior;
 
 create table cnrhelper  as
 select ls.ptid,ls.InstanceUID,ls.Status, ls.diagnosticinterval,ls.SegmentationID,ls.FeatureID,
-       max(CASE WHEN ls.labelID = 3 or ls.labelID = 4 or ls.labelID = 5 THEN ls.mean END)  as tumormean,
+       max(CASE WHEN ls.labelID = 3 THEN ls.mean END)  as tumormeanlr3,
+       max(CASE WHEN ls.labelID = 4 THEN ls.mean END)  as tumormeanlr4,
+       max(CASE WHEN ls.labelID = 5 THEN ls.mean END)  as tumormeanlr5,
        max(CASE WHEN ls.labelID = 6 THEN ls.mean END)  as livermean,
        max(CASE WHEN ls.labelID = 6 THEN ls.stdd END)  as liverstd
 from lstat ls 
-GROUP BY    ls.InstanceUID,ls.FeatureID;
+GROUP BY   ls.InstanceUID,ls.FeatureID;
 
 create table cnrdata  as
-select  ch.*, (ch.tumormean-ch.livermean)/ch.liverstd as cnr 
+select  ch.*, (ch.tumormeanlr3-ch.livermean)/ch.liverstd as cnrlr3 , (ch.tumormeanlr4-ch.livermean)/ch.liverstd as cnrlr4, (ch.tumormeanlr5-ch.livermean)/ch.liverstd as cnrlr5 
 from cnrhelper ch;
 
 .headers on
-select avg(CASE WHEN cd.FeatureID = 'epm'  THEN cd.cnr END)  as epmcnr,
-       avg(CASE WHEN cd.FeatureID = 'art'  THEN cd.cnr END)  as artcnr,
-       avg(CASE WHEN cd.FeatureID = 'ven'  THEN cd.cnr END)  as vencnr
-from cnrdata cd
-where cd.cnr is not NULL;
-select avg(CASE WHEN cd.FeatureID = 'epm'  THEN cd.cnr END)  as epmcnrPreDx,
-       avg(CASE WHEN cd.FeatureID = 'art'  THEN cd.cnr END)  as artcnrPreDx,
-       avg(CASE WHEN cd.FeatureID = 'ven'  THEN cd.cnr END)  as vencnrPreDx
-from cnrdata cd
-where cd.cnr is not NULL and cd.diagnosticinterval > 0;
-select avg(CASE WHEN cd.FeatureID = 'epm'  THEN cd.cnr END)  as epmcnrDx,
-       avg(CASE WHEN cd.FeatureID = 'art'  THEN cd.cnr END)  as artcnrDx,
-       avg(CASE WHEN cd.FeatureID = 'ven'  THEN cd.cnr END)  as vencnrDx
-from cnrdata cd
-where cd.cnr is not NULL and cd.diagnosticinterval = 0;
+create table cnrdatalr3PreDx  as
+select count(CASE WHEN cd.FeatureID = 'epm' THEN cd.cnrlr3 END) as epmcntbPreDxlr3,sum(CASE WHEN cd.FeatureID = 'epm' THEN cd.cnrlr3 END)  as epmsumbPreDxlr3,
+       count(CASE WHEN cd.FeatureID = 'art' THEN cd.cnrlr3 END) as artcntbPreDxlr3,sum(CASE WHEN cd.FeatureID = 'art' THEN cd.cnrlr3 END)  as artsumbPreDxlr3,
+       count(CASE WHEN cd.FeatureID = 'ven' THEN cd.cnrlr3 END) as vencntbPreDxlr3,sum(CASE WHEN cd.FeatureID = 'ven' THEN cd.cnrlr3 END)  as vensumbPreDxlr3
+from cnrdata cd 
+where cd.cnrlr3 is not NULL and cd.diagnosticinterval > 0;
 
+create table cnrdatalr4PreDx  as
+select count(CASE WHEN cd.FeatureID = 'epm' THEN cd.cnrlr4 END) as epmcntbPreDxlr4,sum(CASE WHEN cd.FeatureID = 'epm' THEN cd.cnrlr4 END)  as epmsumbPreDxlr4,
+       count(CASE WHEN cd.FeatureID = 'art' THEN cd.cnrlr4 END) as artcntbPreDxlr4,sum(CASE WHEN cd.FeatureID = 'art' THEN cd.cnrlr4 END)  as artsumbPreDxlr4,
+       count(CASE WHEN cd.FeatureID = 'ven' THEN cd.cnrlr4 END) as vencntbPreDxlr4,sum(CASE WHEN cd.FeatureID = 'ven' THEN cd.cnrlr4 END)  as vensumbPreDxlr4
+from cnrdata cd 
+where cd.cnrlr4 is not NULL and cd.diagnosticinterval > 0;
+
+
+create table cnrdatalr5Dx  as
+select count(CASE WHEN cd.FeatureID = 'epm' THEN cd.cnrlr5 END) as epmcntbDxlr5,sum(CASE WHEN cd.FeatureID = 'epm' THEN cd.cnrlr5 END)  as epmsumbDxlr5,
+       count(CASE WHEN cd.FeatureID = 'art' THEN cd.cnrlr5 END) as artcntbDxlr5,sum(CASE WHEN cd.FeatureID = 'art' THEN cd.cnrlr5 END)  as artsumbDxlr5,
+       count(CASE WHEN cd.FeatureID = 'ven' THEN cd.cnrlr5 END) as vencntbDxlr5,sum(CASE WHEN cd.FeatureID = 'ven' THEN cd.cnrlr5 END)  as vensumbDxlr5
+from cnrdata cd 
+where cd.cnrlr5 is not NULL and cd.diagnosticinterval == 0;
+
+create table cnrdatalr4Dx  as
+select count(CASE WHEN cd.FeatureID = 'epm' THEN cd.cnrlr4 END) as epmcntbDxlr4,sum(CASE WHEN cd.FeatureID = 'epm' THEN cd.cnrlr4 END)  as epmsumbDxlr4,
+       count(CASE WHEN cd.FeatureID = 'art' THEN cd.cnrlr4 END) as artcntbDxlr4,sum(CASE WHEN cd.FeatureID = 'art' THEN cd.cnrlr4 END)  as artsumbDxlr4,
+       count(CASE WHEN cd.FeatureID = 'ven' THEN cd.cnrlr4 END) as vencntbDxlr4,sum(CASE WHEN cd.FeatureID = 'ven' THEN cd.cnrlr4 END)  as vensumbDxlr4
+from cnrdata cd 
+where cd.cnrlr4 is not NULL and cd.diagnosticinterval == 0;
+
+
+
+select * from cnrdatalr3PreDx;
+select * from cnrdatalr4PreDx;
+
+select (plr3.epmsumbPreDxlr3+ plr4.epmsumbPreDxlr4)/( plr3.epmcntbPreDxlr3+ plr4.epmcntbPreDxlr4)  as epmcnrPreDx,
+       (plr3.artsumbPreDxlr3+ plr4.artsumbPreDxlr4)/( plr3.artcntbPreDxlr3+ plr4.artcntbPreDxlr4)  as artcnrPreDx,
+       (plr3.vensumbPreDxlr3+ plr4.vensumbPreDxlr4)/( plr3.vencntbPreDxlr3+ plr4.vencntbPreDxlr4)  as vencnrPreDx 
+from      cnrdatalr3PreDx plr3 join  cnrdatalr4PreDx plr4;
+
+select (plr5.epmsumbDxlr5+ plr4.epmsumbDxlr4)/( plr5.epmcntbDxlr5+ plr4.epmcntbDxlr4)  as epmcnrDx,
+       (plr5.artsumbDxlr5+ plr4.artsumbDxlr4)/( plr5.artcntbDxlr5+ plr4.artcntbDxlr4)  as artcnrDx,
+       (plr5.vensumbDxlr5+ plr4.vensumbDxlr4)/( plr5.vencntbDxlr5+ plr4.vencntbDxlr4)  as vencnrDx 
+from      cnrdatalr5Dx plr5 join  cnrdatalr4Dx plr4;
 
 .output epmstats/widejoin.csv  
 select pl.UID,pl.Status,ls.*
