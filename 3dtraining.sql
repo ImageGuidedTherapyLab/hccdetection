40a3c1623e2bec47845260c12c7297b75b96e993
David Fuentes <fuentesdt@gmail.com>
2025-08-13 10:57:57 -0500

    WIP:

---
+++ 3dtraining.sql
@@ -0,0 +1,24 @@
+
+-- cat 3dtraining.sql  | sqlite3
+-- sqlite3 -init 3dtraining.sql
+.mode csv
+.import bcmlirads/wideanon.csv datakey
+create table patientlist  as
+select substr(dk.UID,1,7) ptid,dk.UID,dk.status from datakey dk where dk.diagnosticinterval = '0.0' or dk.daysincebaseline = '0.0'; 
+
+create table patientlistneg  as
+select substr(dk.UID,1,7) ptid,dk.UID,dk.status,cast(dk.diagnosticinterval as float) diagnosticinterval  from datakey dk where cast(dk.diagnosticinterval as float) > 0.0;
+
+create table patientlistprior  as
+select pn.ptid ptid ,pn.UID UID,pn.status status,min (pn.diagnosticinterval) diagnosticinterval from patientlistneg  pn  group by pn.ptid ;
+
+create table patientlistunion as
+select * from patientlist where status = 'control'  union
+select ptid,UID,status from patientlistprior;
+
+.headers on
+.output 3dtrainingdx.csv
+select *, 'bcmdata/'||UID||'/EPM.nii' image, 'bcmdata/'||UID||'/Art.liver.nii.gz' label from  patientlist;
+.output 3dtrainingpredx.csv
+select *, 'bcmdata/'||UID||'/EPM.nii' image, 'bcmdata/'||UID||'/Art.liver.nii.gz' label from  patientlistunion ;
+.output stdout
